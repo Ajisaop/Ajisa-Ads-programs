77de5fcf337f4e639a0a9f5929f58ef20addc5b1
Ajisaop <151126684+Ajisaop@users.noreply.github.com>
2023-12-25 08:13:08 +0530

    doublylinkedlist.c

--- doublylinkedlist.c
+++ doublylinkedlist.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
-
-struct node{
- int data;
- struct node*prev;
- struct node*next;
+struct node
+{
+int data;
+struct node*prev;
+struct node*next;
 };
-
-struct node *head=NULL,*end=NULL;
+struct node*head=NULL,*end=NULL;
 static int size=0;
-void insert_head(int data){
+void insert_head(int data)
+{
 struct node*node;
-node=(struct node*)malloc(sizeof(struct node));
+node=(struct  node*)malloc(sizeof(struct node));
 node->data=data;
 node->next=head;
 node->prev=NULL;
@@ -20,18 +20,18 @@ if(head!=NULL)
 head->prev=node;
 head=node;
 }
-else
-{
+else{
 head=node;
 end=node;
 }
 size++;
 }
 
-void insert_end(int data){
+void insert_end(int data)
+{
 struct node*node;
 node=(struct node*)malloc(sizeof(struct node));
-node->data;
+node->data=data;
 node->next=NULL;
 node->prev=NULL;
 if(end==NULL)
@@ -47,7 +47,8 @@ end=node;
 size++;
 }
 
-void insert(int data,int pos){
+void insert(int data,int pos)
+{
 int count=1;
 struct node*node,*temp;
 temp=head;
@@ -56,6 +57,8 @@ node->data=data;
 do{
 if(count==pos){
 node->next=temp->next;
+(temp->next)->prev=node;
+node->prev=temp;
 temp->next=node;
 size++;
 break;
@@ -64,14 +67,16 @@ else{
 count++;
 temp=temp->next;
 }
-}while(count<=pos);
+}
+while(count<=pos);
 }
 
-void delete_head(){
-struct node *temp;
+void delete_head()
+{
+struct node*temp;
 if(head!=NULL)
 {
-printf("\nNode deleted:%d,head->data");
+printf("\n node deleted:%d",head->data);
 temp=head;
 head=head->next;
 if(head!=NULL)
@@ -82,14 +87,15 @@ free(temp);
 size--;
 }
 else
-printf("\nLinked list is empty!");
+printf("\nlinked list is empty!");
 }
 
-void delete_end(){
+void delete_end()
+{
 if(head==NULL)
-printf("\nLinked list is empty!");
+printf("\nlinked list is empty!");
 else{
-printf("\nNode deleted:%d",end->data);
+printf("\n node deleted:%d",end->data);
 struct node*temp,*next_temp;
 temp=end;
 end=end->prev;
@@ -101,16 +107,18 @@ free(temp);
 size--;
 }}
 
-void delete(int pos){
+void delete(int pos)
+{
 int count=1;
 struct node*temp,*next_temp;
 temp=head;
 next_temp=temp->next;
 do{
-if(count==pos){
+if(count==pos)
+{
 temp->next=next_temp->next;
 (next_temp->next)->prev=temp;
-printf("\nNode deleted:%d",next_temp->data);
+printf("\n node deleted:%d",next_temp->data);
 free(next_temp);
 size--;
 break;
@@ -119,13 +127,13 @@ else{
 count++;
 temp=next_temp;
 next_temp=next_temp->next;
-}}
-while(count<=pos);
+}
+}while(count<=pos);
 }
 
 void display(){
 if(head==NULL)
-printf("\nLinked list is empty!");
+printf("\nlinked list is empty!");
 else{
 struct node*link=head;
 while(link->next!=NULL){
@@ -133,8 +141,8 @@ printf("%d\t",link->data);
 link=link->next;
 }
 printf("%d\n",link->data);
-}
-}
+}}
+
 
 void search()
 {
